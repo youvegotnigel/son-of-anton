83b91dcc2e2d9c61de26a03845ee68f0b023f8fc
youvegotnigel <nigeldave19@gmail.com>
2020-10-30 12:06:27 +0530

    add ngrok

--- bot.py
+++ bot.py
@@ -2,10 +2,23 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#jenkins-builds', text="Hello world")
+client.chat_postMessage(channel='#jenkins-builds', text="Good Morning")
+
+#client.chat_postMessage(channel='#qaninjas', text="Hello world!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
