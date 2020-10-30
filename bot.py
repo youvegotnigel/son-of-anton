b528d93b63be4ebf1b8e63c9defc0478cd9a606a
youvegotnigel <nigeldave19@gmail.com>
2020-10-30 10:55:02 +0530

    Fix Bugs

--- bot.py
+++ bot.py
@@ -7,3 +7,5 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel='#jenkins-builds', text="Hello world")
